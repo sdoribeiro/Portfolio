38d9b6112619ff1b1c2de63e9bf16181dee330c8
sdoribeiro <sdoribeiro@gmail.com>
2024-06-14 14:54:04 -0400

    adequacoes modelos layout em lista

--- assets/admin.py
+++ assets/admin.py
@@ -3,8 +3,14 @@ from .models import Portfolio, Sector, Asset, Trade, PorfolioComp
 
 # Register your models here.
 
+class TradeAdmin(admin.ModelAdmin):
+    list_display = ("id", "ticker", "date", "quantity", "price", "tax")
+
+class PortfolioCompAdmin(admin.ModelAdmin):
+    list_display = ("id", "portfolio", "asset", "percentage")
+
 admin.site.register(Portfolio)
 admin.site.register(Sector)
 admin.site.register(Asset)
-admin.site.register(Trade)
-admin.site.register(PorfolioComp)
+admin.site.register(Trade, TradeAdmin)
+admin.site.register(PorfolioComp, PortfolioCompAdmin)
