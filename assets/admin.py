490d21a87502066c7ac4ffb7fca64e5792623146
sdoribeiro <sdoribeiro@gmail.com>
2024-06-14 12:51:22 -0400

    Models definitios

--- assets/admin.py
+++ assets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Portfolio, Sector, Asset, Trade, PorfolioComp
 
 # Register your models here.
+
+admin.site.register(Portfolio)
+admin.site.register(Sector)
+admin.site.register(Asset)
+admin.site.register(Trade)
+admin.site.register(PorfolioComp)
