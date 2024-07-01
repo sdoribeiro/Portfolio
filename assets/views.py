ee33d708f3f492d854bcd098fc08b10a55f42052
sdoribeiro <sdoribeiro@gmail.com>
2024-07-01 16:28:07 -0400

    Estudo de classes e models

--- assets/views.py
+++ assets/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Asset, Trade
+
 # Create your views here.
+def index(request):
+    return render(request, "assets/index.html", {
+        "assets": Asset.objects.all(),
+        "trades": Trade.objects.all(),
+        
+    })
