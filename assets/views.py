c190de524c082544fea6527e7c662cf27d3911cf
sdoribeiro <sdoribeiro@gmail.com>
2024-07-09 16:18:21 -0400

    estudos

--- assets/views.py
+++ assets/views.py
@@ -4,6 +4,16 @@ from .models import Asset, Trade
 
 # Create your views here.
 def index(request):
+
+    a1 = Asset.objects.all()
+    a2 = Trade.objects.all()
+
+    for a in a1:
+        print(a.id, a.ticker)
+
+    for b in a2:
+        print(b.id, b.ticker)
+
     return render(request, "assets/index.html", {
         "assets": Asset.objects.all(),
         "trades": Trade.objects.all(),
