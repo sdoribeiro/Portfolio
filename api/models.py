36b5541cdb8a9e80b3faedc250a5399cbd545d12
sdoribeiro <sdoribeiro@gmail.com>
2024-08-05 10:32:48 -0400

    dados dinamicos para composicao de graficos

--- api/models.py
+++ api/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 class TotalViewsModel(models.Model):
     label = models.CharField(max_length=64)
     views = models.IntegerField()
+
+class MostWatchedVideos(models.Model):
+    title = models.CharField(max_length=64)
+    published_date = models.DateTimeField(default=datetime.now())
+    views_count = models.IntegerField(default=0)
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from api.models import TotalViewsModel
+from api.models import TotalViewsModel, MostWatchedVideos
 
 # Create your views here.
 
@@ -18,3 +18,19 @@ def totalViews(request):
         dct["data"].append(item.views)
 
     return JsonResponse(dct)
+
+def datatable_api(request):
+    queryset= MostWatchedVideos.objects.all()
+
+    dct = {
+        "title": [],
+        "published_date": [],
+        "views_count": []
+    }
+
+    for item in queryset:
+        dct["title"].append(item.title)
+        dct["published_date"].append(item.published_date)
+        dct["views_count"].append(item.views_count)
+    return JsonResponse(dct)
+
